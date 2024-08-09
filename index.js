122834061d993fcb2224ca6a268ee4a1b014a029
Chien <chientrungha2004@gmail.com>
2024-08-09 09:45:49 +0700

    dựng layout chung

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require("dotenv").config();
 
 const database = require("./config/database");
 
+const systemConfig = require("./config/system");
+
+const routeAdmin = require("./routes/admin/index.route");
 const route = require("./routes/client/index.route");
 
 database.connect();
@@ -13,9 +16,13 @@ const port = process.env.PORT;
 app.set("views", "./views");
 app.set("view engine", "pug");
 
+// App local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin
+
 app.use(express.static("public"));
 
 //Routes
+routeAdmin(app);
 route(app);
 
 app.listen(port, () => {
