10649684b63dd657618bf294edf64b19b64be1c1
Chien <chientrungha2004@gmail.com>
2024-08-10 21:16:27 +0700

    tính năng tìm kiếm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,43 +2,53 @@ const Product = require("../../models/product.model");
 
 //[GET] /admin/products
 module.exports.index = async (req, res) => {
-// console.log(req.query.status);
+  // console.log(req.query.status);
 
   let filterStatus = [
     {
       name: "Tất cả",
       status: "",
-      class: ""
+      class: "",
     },
     {
       name: "Hoạt động",
       status: "active",
-      class: ""
+      class: "",
     },
     {
       name: "Dừng hoạt động",
       status: "inactive",
-      class: ""
-    }
+      class: "",
+    },
   ];
 
-  if(req.query.status){
-      const index = filterStatus.findIndex(item => item.status == req.query.status);
-      filterStatus[index].class = "active";
+  if (req.query.status) {
+    const index = filterStatus.findIndex(
+      (item) => item.status == req.query.status
+    );
+    filterStatus[index].class = "active";
   } else {
-      const index = filterStatus.findIndex(item => item.status == "");
-      filterStatus[index].class = "active";
+    const index = filterStatus.findIndex((item) => item.status == "");
+    filterStatus[index].class = "active";
   }
 
   // console.log(req.query.status);
 
   let find = {
-    deleted: false
+    deleted: false,
   };
-  if(req.query.status){
+
+  if (req.query.status) {
     find.status = req.query.status;
   }
-  
+
+  let keyword = "";
+
+  if (req.query.keyword) {
+    keyword = req.query.keyword;
+    const regex = new RegExp(keyword,"i");
+    find.title = keyword;
+  }
 
   const products = await Product.find(find);
   // const products = await Product.find({
@@ -50,6 +60,7 @@ module.exports.index = async (req, res) => {
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
-    filterStatus: filterStatus
+    filterStatus: filterStatus,
+    keyword: keyword,
   });
-}
+};
