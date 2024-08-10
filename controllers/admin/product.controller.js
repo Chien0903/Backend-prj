8a2cc8b8f1e4c78d662bf3eabf72c622e0747f6a
Chien <chientrungha2004@gmail.com>
2024-08-11 02:02:47 +0700

    phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,6 +3,8 @@ const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 
 const searchHelper = require("../../helpers/search");
+
+const paginationHelper = require("../../helpers/pagination");
 //[GET] /admin/products
 module.exports.index = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
@@ -23,12 +25,36 @@ module.exports.index = async (req, res) => {
     find.title = objectSearch.regex;
   }
 
-  const products = await Product.find(find);
+  //Pagination
+      
+  const countProducts = await Product.countDocuments(find);
+  let objectPagination = paginationHelper (
+    {
+      currentPage: 1,
+      limitItem: 4
+    },
+    req.query,
+    countProducts
+  );
+  
+  // if(req.query.page) {
+  //   objectPagination.currentPage = parseInt(req.query.page);
+  // }
+
+  // objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
+
+  // const countProducts = await Product.countDocuments(find);
+  // const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
+  // objectPagination.totalPage = totalPage;
+  //End Pagination
+
+  const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
-    keyword: objectSearch.keyword
+    keyword: objectSearch.keyword,
+    pagination: objectPagination
   });
 };
