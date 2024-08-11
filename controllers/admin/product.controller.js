86a514f549c63bcba74d5ff03d4d28c389cc3df9
Chien <chientrungha2004@gmail.com>
2024-08-12 02:12:06 +0700

    xóa sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -77,7 +77,10 @@ module.exports.changeMulti = async (req, res) => {
 //[GET] /admin/products/delete/:id
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
-  await Product.deleteOne({ _id: id });
+  await Product.updateOne({ _id: id }, {
+    deleted: true,
+    deletedat: new Date()
+  });
   res.redirect("back");
 };
 
