f44c41c72813c7355523ef586b756b9622f9bbc2
Chien <chientrungha2004@gmail.com>
2024-08-12 01:02:40 +0700

    cập nhật trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,10 +1,8 @@
 const Product = require("../../models/product.model");
-
 const filterStatusHelper = require("../../helpers/filterStatus");
-
 const searchHelper = require("../../helpers/search");
-
 const paginationHelper = require("../../helpers/pagination");
+const { query } = require("express");
 //[GET] /admin/products
 module.exports.index = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
@@ -17,44 +15,61 @@ module.exports.index = async (req, res) => {
     find.status = req.query.status;
   }
 
-// Tìm kiếm
+  // Tìm kiếm
 
   const objectSearch = searchHelper(req.query);
 
-  if(objectSearch.regex) {
+  if (objectSearch.regex) {
     find.title = objectSearch.regex;
   }
 
   //Pagination
-      
+
   const countProducts = await Product.countDocuments(find);
-  let objectPagination = paginationHelper (
+  let objectPagination = paginationHelper(
     {
       currentPage: 1,
-      limitItem: 4
+      limitItem: 4,
     },
     req.query,
     countProducts
   );
-  
-  // if(req.query.page) {
-  //   objectPagination.currentPage = parseInt(req.query.page);
-  // }
 
-  // objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItem;
-
-  // const countProducts = await Product.countDocuments(find);
-  // const totalPage = Math.ceil(countProducts/objectPagination.limitItem);
-  // objectPagination.totalPage = totalPage;
-  //End Pagination
-
-  const products = await Product.find(find).limit(objectPagination.limitItem).skip(objectPagination.skip);
+  const products = await Product.find(find)
+    .limit(objectPagination.limitItem)
+    .skip(objectPagination.skip);
 
   res.render("admin/pages/products/index.pug", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
     keyword: objectSearch.keyword,
-    pagination: objectPagination
+    pagination: objectPagination,
   });
+};
+
+//[GET] /admin/products/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+  const status = req.params.status;
+  const id = req.params.id;
+
+  await Product.updateOne({ _id: id }, { status: status });
+  console.log(id);
+  res.redirect("back");
+};
+
+module.exports.changeMulti = async (req, res) => {
+  const type = req.body.type;
+  const ids = req.body.ids.split(", ");
+  switch (type) {
+    case "active":
+      await Product.updateMany({_id: {$in: ids}}, {status: "active"});
+      break;
+    case "inactive":
+      await Product.updateMany({_id: {$in: ids}}, {status: "inactive"});
+      break;
+    default:
+
+      break;
+  }
 };
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,52 +1,110 @@
 //button status
 const buttonsStatus = document.querySelectorAll("[button-status]");
-if(buttonsStatus.length > 0){
-    let url = new URL(window.location.href);
-    
-    buttonsStatus.forEach(button => {
-        button.addEventListener("click",() => {
-        const status = button.getAttribute("button-status");
-        if(status){
-            url.searchParams.set("status",status);
-        } else{
-            url.searchParams.delete("status");
-        }
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
 
-        console.log(url.href);
-        window.location.href = url.href;
-        });
+  buttonsStatus.forEach((button) => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+
+      console.log(url.href);
+      window.location.href = url.href;
     });
+  });
 }
 //End button status
 
 // Form search
 const formSearch = document.querySelector("#form-search");
-if(formSearch) {
-    let url = new URL(window.location.href);
-    formSearch.addEventListener("submit",(e) => {
-        e.preventDefault();
-        const keyword = e.target.elements.keyword.value;
-        if(keyword){
-            url.searchParams.set("keyword",keyword);
-        } else{
-            url.searchParams.delete("keyword");
-        }
-        window.location.href = url.href;
-    });
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  });
 }
 // End Form search
 
 // Pagination
 const buttonsPagination = document.querySelectorAll("[button-pagination]");
-if(buttonsPagination){
-    let url = new URL(window.location.href);
-    buttonsPagination.forEach(button => {
-        button.addEventListener("click", () => {
+if (buttonsPagination) {
+  let url = new URL(window.location.href);
+  buttonsPagination.forEach((button) => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+      url.searchParams.set("page", page);
+      window.location.href = url.href;
+    });
+  });
+}
+//End
 
-            const page = button.getAttribute("button-pagination");
-            url.searchParams.set("page", page);
-            window.location.href = url.href;
-        });
+//Checkbox Multi
+const checkboxMulti = document.querySelector("[checkbox-multi]");
+if (checkboxMulti) {
+  const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+  const inputIds = checkboxMulti.querySelectorAll("input[name='id']");
+  inputCheckAll.addEventListener("click", () => {
+    if (inputCheckAll.checked) {
+      inputIds.forEach((input) => {
+        input.checked = true;
+      });
+    } else {
+      inputIds.forEach((input) => {
+        input.checked = false;
+      });
+    }
+  });
+  inputIds.forEach((input) => {
+    input.addEventListener("click", () => {
+      const countChecked = checkboxMulti.querySelectorAll(
+        "input[name='id']:checked"
+      ).length;
+      console.log(countChecked);
+      console.log(inputIds.length);
+      if(countChecked == inputIds.length){
+        inputCheckAll.checked = true;
+      } else {
+        inputCheckAll.checked = false;
+      }
+    });
+  });
+}
+//End Checkbox Multi
+
+// Form Change Multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if(formChangeMulti){
+    formChangeMulti.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const checkboxMulti = document.querySelector("[checkbox-multi]");
+        const inputChecked = checkboxMulti.querySelectorAll(
+            "input[name='id']:checked"
+          );
+          if(inputChecked.length > 0) {
+            let ids= [];
+            const inputIds = formChangeMulti.querySelector("input[name='ids']");
+
+            inputChecked.forEach(input => {
+                const id = input.value;
+                ids.push(id);
+            });
+            inputIds.value =ids.join(", ");
+            formChangeMulti.submit();
+          } else {
+            alert("Vui lòng chọn ít nhất một bản ghi!");
+          }
     });
 }
-//End Pagination
+// End Form Change Multi
