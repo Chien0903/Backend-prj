dabc26f6e20a5a6482fb24f0ff50770de67b11e3
Chien <chientrungha2004@gmail.com>
2024-08-13 20:38:29 +0700

    tính năng tạo mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -3,6 +3,7 @@ const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
 const paginationHelper = require("../../helpers/pagination");
 const { query } = require("express");
+const systemConfig = require("../../config/system");
 //[GET] /admin/products
 module.exports.index = async (req, res) => {
   const filterStatus = filterStatusHelper(req.query);
@@ -36,7 +37,7 @@ module.exports.index = async (req, res) => {
   );
 
   const products = await Product.find(find)
-    .sort({ position: "desc"})
+    .sort({ position: "desc" })
     .limit(objectPagination.limitItem)
     .skip(objectPagination.skip);
 
@@ -64,28 +65,38 @@ module.exports.changeMulti = async (req, res) => {
   const ids = req.body.ids.split(", ");
   switch (type) {
     case "active":
-      await Product.updateMany({_id: {$in: ids}}, {status: "active"});
-      req.flash("success", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+      await Product.updateMany({ _id: { $in: ids } }, { status: "active" });
+      req.flash(
+        "success",
+        `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`
+      );
       break;
     case "inactive":
-      await Product.updateMany({_id: {$in: ids}}, {status: "inactive"});
-      req.flash("success", `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`);
+      await Product.updateMany({ _id: { $in: ids } }, { status: "inactive" });
+      req.flash(
+        "success",
+        `Cập nhật trạng thái thành công ${ids.length} sản phẩm!`
+      );
       break;
     case "delete-all":
-      await Product.updateMany({_id: {$in: ids}}, 
+      await Product.updateMany(
+        { _id: { $in: ids } },
         {
           deleted: true,
-          deletedAt: new Date()
+          deletedAt: new Date(),
         }
-      )
+      );
       break;
     case "change-position":
       for (const item of ids) {
-        let [id,position] = item.split("-");
+        let [id, position] = item.split("-");
         position = parseInt(position);
-        await Product.updateOne({_id:id},{
-          position: position
-        });
+        await Product.updateOne(
+          { _id: id },
+          {
+            position: position,
+          }
+        );
       }
       break;
     default:
@@ -96,10 +107,36 @@ module.exports.changeMulti = async (req, res) => {
 //[GET] /admin/products/delete/:id
 module.exports.deleteItem = async (req, res) => {
   const id = req.params.id;
-  await Product.updateOne({ _id: id }, {
-    deleted: true,
-    deletedat: new Date()
-  });
+  await Product.updateOne(
+    { _id: id },
+    {
+      deleted: true,
+      deletedat: new Date(),
+    }
+  );
+  req.flash("success", `Đã xóa thành công sản phẩm!`);
   res.redirect("back");
 };
 
+//[GET] /admin/products/create
+module.exports.create = async (req, res) => {
+  res.render("admin/pages/products/create", {
+    pageTitle: "Thêm mới sản phẩm",
+  });
+};
+
+//[POST] /admin/products/create
+module.exports.createPost = async (req, res) => {
+  req.body.price = parseInt(req.body.price);
+  req.body.discountPecentage = parseInt(req.body.discountPecentage);
+  req.body.stock = parseInt(req.body.stock);
+  if (req.body.position == "") {
+    const countProducts = await Product.countDocuments();
+    req.body.position = countProducts + 1;
+  } else {
+    req.body.position = parseInt(req.body.position);
+  }
+  const product = new Product(req.body);
+  await product.save();
+  res.redirect(`${systemConfig.prefixAdmin}/products`);
+};
