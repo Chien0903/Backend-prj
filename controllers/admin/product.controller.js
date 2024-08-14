1b7d29246f6b3e8cf987f4f0829404e79bc0add1
Chien <chientrungha2004@gmail.com>
2024-08-14 10:35:58 +0700

    validate data

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -127,6 +127,11 @@ module.exports.create = async (req, res) => {
 
 //[POST] /admin/products/create
 module.exports.createPost = async (req, res) => {
+  if(!req.body.title) {
+    req.flash("error", `Vui lòng nhập tiêu đề!`);
+    res.redirect("back");
+    return;
+  }
   req.body.price = parseInt(req.body.price);
   req.body.discountPecentage = parseInt(req.body.discountPecentage);
   req.body.stock = parseInt(req.body.stock);
@@ -136,8 +141,13 @@ module.exports.createPost = async (req, res) => {
   } else {
     req.body.position = parseInt(req.body.position);
   }
-  req.body.thumbnail = `/uploads/${req.file.filename}`;
+
+  if(req.file){
+      req.body.thumbnail = `/uploads/${req.file.filename}`;
+  }
+
   const product = new Product(req.body);
   await product.save();
+
   res.redirect(`${systemConfig.prefixAdmin}/products`);
 };
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const router = express.Router();
 const storaMulter = require("../../helpers/storageMulter");
 const controller = require("../../controllers/admin/product.controller");
+const validate = require("../../validates/admin/product.validate");
 const storageMulter = require("../../helpers/storageMulter");
 const route = express.Router();
 const upload = multer({ storage: storageMulter() });
@@ -13,6 +14,11 @@ router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
 router.post("/create", upload.single("thumbnail"), controller.createPost);
-router.post("/create",upload.single("thumbnail"),controller.createPost);
+router.post(
+    "/create",
+    upload.single("thumbnail"),
+    validate.createPost,
+    controller.createPost
+);
 
 module.exports = router;
