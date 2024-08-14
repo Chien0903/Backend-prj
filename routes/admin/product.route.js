8d6d017368e0be8236732fdc991e412e402924f0
Chien <chientrungha2004@gmail.com>
2024-08-14 07:00:21 +0700

    hoàn thiện tính năng tạo sản phẩm

--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
+const storaMulter = require("../../helpers/storageMulter");
 const controller = require("../../controllers/admin/product.controller");
+const storageMulter = require("../../helpers/storageMulter");
+const route = express.Router();
+const upload = multer({ storage: storageMulter() });
 
 router.get("/", controller.index);
-router.patch("/change-status/:status/:id",controller.changeStatus);
+router.patch("/change-status/:status/:id", controller.changeStatus);
 router.patch("/change-multi", controller.changeMulti);
 router.delete("/delete/:id", controller.deleteItem);
 router.get("/create", controller.create);
-router.post("/create", controller.createPost);
-module.exports = router;
+router.post("/create", upload.single("thumbnail"), controller.createPost);
+router.post("/create",upload.single("thumbnail"),controller.createPost);
 
+module.exports = router;
