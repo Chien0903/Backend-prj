cb6d87b9f03d474975d671f6965a27b4dccbe867
Chien <chientrungha2004@gmail.com>
2024-08-30 15:53:58 +0700

    tinyMCE

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 const methodOverride = require("method-override");
 const bodyParser =require("body-parser");
 const flash = require("express-flash");
@@ -30,6 +31,9 @@ app.use(session({cookie: {maxAge: 60000}}));
 app.use(flash());
 // End flash
 
+// TinyMCE
+app.use('/tinymce', express.static(path.join(__dirname, 'node_modules', 'tinymce')));
+// End TinyMCE
 // App local Variables
 app.locals.prefixAdmin = systemConfig.prefixAdmin
 
