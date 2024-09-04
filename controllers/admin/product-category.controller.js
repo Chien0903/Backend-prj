42262111ba2517061124c32e4bcaf25a8136b6f0
Chien <chientrungha2004@gmail.com>
2024-09-05 02:09:05 +0700

    chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -66,3 +66,66 @@ module.exports.createPost = async (req, res) => {
   await record.save();
   res.redirect(`${systemConfig.prefixAdmin}/products-category`);
 };
+
+module.exports.edit = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const data = await ProductCategory.findOne({
+      _id: id,
+      deleted: false
+    });
+    find = {
+      deleted: false,
+    };
+    function createTree(arr, parentId = ""){
+      const tree = [];
+      arr.forEach((item) => {
+        if(item.parent_id === parentId) {
+          const newItem = item;
+          const children = createTree(arr, item.id);
+          if(children.length > 0)
+          {
+            newItem.children = children;
+          }
+          tree.push(newItem);
+        }
+      });
+      return tree;
+    }
+    const records = await ProductCategory.find(find);
+    const newRecords = createTree(records);
+    console.log(newRecords);
+    res.render("admin/pages/products-category/edit.pug", {
+      pageTitle: "Chỉnh sửa danh mục sản phẩm",
+      data: data,
+      records: newRecords
+    });
+  } catch (error) {
+    res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+  }
+ 
+};
+
+module.exports.editPatch = async (req,res) => {
+  const id = req.params.id
+  req.body.position = parseInt(req.body.position);
+  try {
+    await ProductCategory.updateOne({_id: id}, req.body);
+    req.flash("Updated Successfull")
+  } catch (error) {
+    req.flash("Updated Error")
+  }
+  res.redirect(`${systemConfig.prefixAdmin}/products-category`);
+};
+
+module.exports.detail = async (req,res) => {
+  const find = {
+    deleted: false,
+    _id: req.params.id
+  };
+  const product = await ProductCategory.findOne(find);
+  res.render("admin/pages/products-category/detail" ,{
+    pageTitle: product.title,
+    product: product
+  });
+};
