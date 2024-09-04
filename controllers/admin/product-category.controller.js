8f09efd83eb0b6747f1f104322601dfa2db28c6b
Chien <chientrungha2004@gmail.com>
2024-09-05 02:08:38 +0700

    chỉnh sửa danh mục sản phẩm

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -5,16 +5,53 @@ module.exports.index = async (req, res) => {
   let find = {
     deleted: false,
   };
-  const records = await ProductCategory.find(find)
+  function createTree(arr, parentId = ""){
+    const tree = [];
+    arr.forEach((item) => {
+      if(item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if(children.length > 0)
+        {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+  const records = await ProductCategory.find(find);
+  const newRecords = createTree(records);
   res.render("admin/pages/products-category/index.pug", {
     pageTitle: "Danh mục sản phẩm",
-    records: records
+    records: newRecords
   });
 };
 //[GET] /admin/products-category/create
 module.exports.create = async (req, res) => {
+  find = {
+    deleted: false,
+  };
+  function createTree(arr, parentId = ""){
+    const tree = [];
+    arr.forEach((item) => {
+      if(item.parent_id === parentId) {
+        const newItem = item;
+        const children = createTree(arr, item.id);
+        if(children.length > 0)
+        {
+          newItem.children = children;
+        }
+        tree.push(newItem);
+      }
+    });
+    return tree;
+  }
+  const records = await ProductCategory.find(find);
+  const newRecords = createTree(records);
   res.render("admin/pages/products-category/create.pug", {
     pageTitle: "Tạo danh mục sản phẩm",
+    records: newRecords
   });
 };
 
