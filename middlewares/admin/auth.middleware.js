100d16b07ea017575cd979bc801888eaf832a590
Chien <chientrungha2004@gmail.com>
2024-09-15 23:03:48 +0700

    middlewares

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -5,7 +5,6 @@ module.exports.requireAuth = async (req ,res ,next) =>{
     if(!req.cookies.token){
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
-        console.log(req.cookies.token)
         const user = await Account.findOne({token: req.cookies.token})
         if(!user){
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
