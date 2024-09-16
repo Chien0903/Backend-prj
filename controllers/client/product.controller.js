20b6e8530e46b8af7455b46fb1449be795b17b5e
Chien <chientrungha2004@gmail.com>
2024-09-17 02:36:24 +0700

    trang chi tiết bên client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,32 +1,37 @@
 const Product = require("../../models/product.model");
-
+const ProductCategory = require("../../models/product-category.model");
+const productsHelper = require("../../helpers/product");
+const productCategoryHelper = require("../../helpers/product-category");
 //[GET] /products
-
 module.exports.index = async (req, res) => {
   const products = await Product.find({
     status: "active",
     deleted: "false",
   }).sort({position:"desc"});
-
-const newProducts = products.map(item => {
-  item.priceNew =  (item.price * (100 - item.discountPercentage)/100).toFixed(0);
-  return item;
-})
-
+const newProducts = productsHelper.priceNewProducts(products);
 res.render("client/pages/products/index.pug", {
     pageTitle: "Danh sach san pham",
     products: newProducts,
   });
 };
-
+// [GET] /products/:slugProduct
 module.exports.detail = async (req,res) => {
   try{
     const find = {
       deleted: false,
-      slug: req.params.slug,
+      slug: req.params.slugProduct,
       status: "active"
     };
     const product = await Product.findOne(find);
+    if(product.product_category_id) {
+      const category = await ProductCategory.findOne({
+        _id: product.product_category_id,
+        status: "active",
+        deleted: false
+      })
+      product.category = category;
+    }
+    product.priceNew = productsHelper.priceNewProduct(product);
     res.render("client/pages/products/detail" ,{
       pageTitle: product.title,
       product: product,
@@ -34,4 +39,24 @@ module.exports.detail = async (req,res) => {
   } catch (error) {
     res.redirect(`/products`);
   };
+};
+// [GET] /products/:slugCategory
+module.exports.category = async (req,res) => {
+  const category = await ProductCategory.findOne({
+    slug: req.params.slugCategory,
+    deleted: false
+  });
+  console.log(category);
+  const listSubCategory = await productCategoryHelper.getSubCategory(category.id);
+  const listSubCategoryId = listSubCategory.map(item => item.id);
+  const products = await Product.find({
+    product_category_id: { $in: [category.id, ...listSubCategoryId]},
+    deleted: false
+  }).sort({position: "desc"});
+  const newProducts = productsHelper.priceNewProducts(products);
+  console.log(products);
+  res.render("client/pages/products/index", {
+    pageTitle: category.title,
+    products: newProducts
+  });
 };
