2e172bdd7471b0edff28ef570f33938bd169f3c7
Chien <chientrungha2004@gmail.com>
2024-09-17 22:38:42 +0700

    tìm kiếm sản phẩm

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,13 @@
 const homeRoutes = require("./home.route");
 const productRoutes = require("./product.route");
+const searchRoutes = require("./search.route")
 const categoryMiddleware = require("../../middlewares/client/category.middleware");
 module.exports = (app) => {
-  app.use(categoryMiddleware.category,)
+  app.use(categoryMiddleware.category);
 
   app.use("/", homeRoutes);
 
   app.use("/products", productRoutes);
+
+  app.use("/search", searchRoutes);
 };
